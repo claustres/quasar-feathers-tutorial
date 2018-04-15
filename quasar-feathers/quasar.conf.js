9abd2fa0b8429abb7cb487d64837e12eb35a010c
leob <leobdev@gmail.com>
2018-04-15 22:40:08 +0800

    Github issue #11: changed router mode from 'history' to 'hash'

--- quasar-feathers/quasar.conf.js
+++ quasar-feathers/quasar.conf.js
@@ -22,7 +22,7 @@ module.exports = function (ctx) {
     },
     build: {
       scopeHoisting: true,
-      vueRouterMode: 'history',   //'hash',
+      vueRouterMode: 'hash',  //'history',   //'hash',
       // gzip: true,
       // analyze: true,
       // extractCSS: false,
